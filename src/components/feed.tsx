337751e6041f6651f13c7582fbe43313b1910167
minhoo03 <minhoo0333@gmail.com>
2024-02-07 03:54:30 +0900

    글 업데이트 메인 기능 구현

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -1,8 +1,9 @@
 import { styled } from "styled-components";
 import { IFeed } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -42,8 +43,67 @@ const DeleteButton = styled.button`
   cursor: pointer;
 `;
 
+const UpdateButton = styled.button`
+  background-color: #015bd6;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 1px solid #e1e1e1;
+  padding: 12px;
+  border-radius: 8px;
+  font-size: 16px;
+  color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+
+
 export default function Feed({ username, photo, feed, userId, id }: IFeed) {
+
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const [feedText, setFeedText] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
   const user = auth.currentUser;
+
   const onDelete = async () => {
     const ok = confirm("선택한 피드를 삭제하시겠습니까?");
     if (!ok || user?.uid !== userId) return;
@@ -59,14 +119,73 @@ export default function Feed({ username, photo, feed, userId, id }: IFeed) {
       // setToast
     }
   };
+
+  const handleChangeUpdateState = () => {
+    setIsUpdating(prev => !prev)
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFeedText(e.target.value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    // 폼 입력 데이터가 있는 경우 (조건 적합)
+    if (!user || isLoading || feed === "" || feed.length > 180) return;
+    try {
+      setLoading(true);
+      await updateDoc(doc(db, "feeds", id), {
+        feed: feedText,
+        userId: user.uid,
+      });
+
+      // 리셋
+      setFeedText(""); 
+      handleChangeUpdateState()
+
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{feed}</Payload>
+
+        {
+          isUpdating ?
+            <Form onSubmit={onSubmit}>
+              <TextArea
+                required // 빈 값 허용하지 않음
+                rows={5}
+                maxLength={180}
+                onChange={onChange}
+                value={feedText}
+                placeholder="What is happening?!"
+              />
+              <SubmitBtn
+                type="submit"
+                value={isLoading ? "Posting..." : "Post Feed"}
+              />
+            </Form>
+            :
+           <Payload>{feed}</Payload>
+        }
+        
         {user?.uid === userId ? ( // 작성자만 삭제 버튼이 보이도록
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          <div style={{display: "flex", gap: 4}}>
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+            <UpdateButton onClick={handleChangeUpdateState}>Update</UpdateButton>
+          </div>
         ) : null}
+
+          
+          
       </Column>
       {photo ? (
         <Column>
