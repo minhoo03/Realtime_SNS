501fcef6acb217fd9708dbe960c904fdc3a60310
minhoo03 <minhoo0333@gmail.com>
2024-02-07 03:31:45 +0900

    My Feed

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,8 +1,18 @@
 import { styled } from "styled-components";
-import { auth, storage } from "../firebase";
-import { useState } from "react";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { IFeed } from "../components/timeline";
+import Feed from "../components/feed";
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,6 +20,14 @@ const Wrapper = styled.div`
   flex-direction: column;
   gap: 20px;
 `;
+
+const Feeds = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 10px;
+`;
+
 const AvatarUpload = styled.label`
   width: 80px;
   overflow: hidden;
@@ -38,6 +56,7 @@ const Name = styled.span`
 export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL); // 로그인된 유저의 사진 URL을 초기값으로
+  const [feeds, setFeeds] = useState<IFeed[]>([]);
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     // 파일 탐색기에서 얻어온 file
     const { files } = e.target;
@@ -58,13 +77,41 @@ export default function Profile() {
       // 이미지 URL 생성
       const avatarUrl = await getDownloadURL(result.ref);
       setAvatar(avatarUrl);
-      
+
       // 유저 프로필 업데이트 (auth)
       await updateProfile(user, {
         photoURL: avatarUrl,
       });
     }
   };
+
+  const fetchFeeds = async () => {
+    const feedQuery = query( 
+      collection(db, "feeds"), // feeds DB
+      where("userId", "==", user?.uid), // 현재 로그인한 계정의 uid와 같은
+      orderBy("createdAt", "desc"),
+      limit(25)
+    );
+
+    const snapshot = await getDocs(feedQuery); // 쿼리대로 DB 조회
+    const feeds = snapshot.docs.map((doc) => { // 조회한 결과 재정의
+      const { feed, createdAt, userId, username, photo } = doc.data();
+      return {
+        feed,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setFeeds(feeds);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
@@ -88,6 +135,12 @@ export default function Profile() {
         accept="image/*"
       />
       <Name>{user?.displayName ?? "Anonymous"}</Name>
+
+      <Feeds>
+        {feeds.map((feed) => (
+          <Feed key={feed.id} {...feed} />
+        ))}
+      </Feeds>
     </Wrapper>
   );
 }
